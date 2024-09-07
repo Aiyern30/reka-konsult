345df8f2bc2776f6d3383439e12e960a5b76af0d
Aiyern30 <ianbian2@gmail.com>
2024-09-07 22:17:40 +0800

    build homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,137 @@
+// page.tsx
+
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+    <>
+      <section
+        id="hero"
+        className="relative flex flex-col items-center justify-center bg-zinc-50 dark:bg-zinc-900 text-slate-950 transition-bg"
+        style={{ height: "calc(100vh - 32px)" }}
+      >
+        <div className="absolute inset-0 overflow-hidden">
+          <div className='[--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)] [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)] [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)] [background-image:var(--white-gradient),var(--aurora)] dark:[background-image:var(--dark-gradient),var(--aurora)] [background-size:300%,_200%] [background-position:50%_50%,50%_50%] filter blur-[10px] invert dark:invert-0 after:content-[""] after:absolute after:inset-0 after:[background-image:var(--white-gradient),var(--aurora)] after:dark:[background-image:var(--dark-gradient),var(--aurora)] after:[background-size:200%,_100%] after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference pointer-events-none absolute -inset-[10px] opacity-50 will-change-transform [mask-image:radial-gradient(ellipse_at_100%_0%,black_10%,var(--transparent)_70%)]'></div>
+        </div>
+        <div
+          className="relative flex flex-col gap-4 items-center justify-center px-4 text-center"
+          style={{ opacity: 1, transform: "none", willChange: "auto" }}
+        >
+          <div className="text-3xl md:text-6xl font-bold dark:text-white">
+            ARKITEK REKAKONSULT SDN BHD
+          </div>
+          <div className="font-extralight text-base md:text-2xl dark:text-neutral-200 py-4">
+            together with .
+          </div>
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://www.canva.com/design/DAGMGgnm47E/l4ks1Tk5hTTmbVnjGYv-rA/view?utm_content=DAGMGgnm47E&utm_campaign=designshare&utm_medium=link&utm_source=editor"
             target="_blank"
             rel="noopener noreferrer"
+            className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+            Get to know more
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <section id="features" className="px-4 lg:px-10 py-20">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <div className="text-3xl font-bold dark:text-white">What we do</div>
+          <div className="grid gap-y-8 gap-x-8 sm:grid-cols-2 lg:grid-cols-3">
+            <Card className="w-80">
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+
+            <Card className="w-80">
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+
+            <Card className="w-80">
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </section>
+      <section id="features" className="px-4 lg:px-10 py-20">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <div className="text-3xl font-bold dark:text-white">
+            The Directors
+          </div>
+          <div className="grid gap-y-8 gap-x-16 sm:grid-cols-1 lg:grid-cols-2 pt-4">
+            <div className="flex flex-col justify-center items-center text-center">
+              <Avatar className="w-80 h-80 border-4 border-black mb-4">
+                <AvatarImage src="/user1.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="font-bold text-xl lg:text-2xl mb-2">
+                AR. Yeo Soon Teck
+              </div>
+              <p className="text-gray-700 px-4 text-justify">
+                AR. Yeo Soon Teck has a distinguished career in architecture,
+                having worked on a diverse range of projects from high-rise
+                offices to national monuments. His experience spans across YRM
+                Architects, Jabatan Kerja Raya, T.R. Hamzah & Yeang, and
+                Kumpulan Perunding, before founding Reka Konsult and leading
+                Arkitek Rekakonsult Sdn Bhd as Director.
+              </p>
+            </div>
+            <div className="flex flex-col justify-center items-center text-center">
+              <Avatar className="w-80 h-80 border-4 border-black mb-4">
+                <AvatarImage src="/user2.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <div className="font-bold text-xl lg:text-2xl mb-2">
+                AR. Mazli Bin Md. Lajis
+              </div>
+              <p className="text-gray-700 px-4 text-justify">
+                AR. Mazli Bin Md. Lajis has a rich background in architectural
+                design and project management. From his early work at Juhari
+                Hashim Arkitek to significant roles at RDP Architect and Antara
+                Akitek Sdn Bhd, he has led major projects and design
+                initiatives. He joined Arkitek Rekakonsult Sdn Bhd in 2002,
+                where he is now a Director.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className="flex flex-wrap items-center justify-between px-4 lg:px-10 py-4 bg-white shadow-lg sticky top-0 z-50 text-black">
+      <a href="#" className="text-[20px] lg:text-[32px] font-semibold">
+        Reka Konsult
+      </a>
+
+      {/* Mobile Menu Button */}
+      <button
+        className="block lg:hidden text-xl px-4 py-2 hover:outline-none"
+        onClick={toggleMenu}
+      >
+        ☰
+      </button>
+
+      {/* Menu Items */}
+      <div
+        className={`w-full lg:flex lg:items-center lg:w-auto ${
+          isMenuOpen ? "block" : "hidden"
+        } mt-6 lg:mt-0`}
+      >
+        <ul className="flex flex-col lg:flex-row lg:space-x-14 space-y-4 lg:space-y-0 text-[18px] lg:items-center">
+          <li className="hover:text-blue-500 hover:font-semibold">
+            <a href="#aboutUs">About</a>
+          </li>
+          <li className="hover:text-blue-500 hover:font-semibold">
+            <a href="/Projects">Projects</a>
+          </li>
+          <li className="hover:text-blue-500 hover:font-semibold">
+            <a href="#howItWorks">How it works</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
