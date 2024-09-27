06afa84dad6bae8610c5770d602e7038549772d3
Aiyern30 <ianbian2@gmail.com>
2024-09-27 19:34:34 +0800

    change the pdf to render image, and refactoring the code in RegisterList componensts

--- components/pages/Projects/RegisterList.tsx
+++ components/pages/Projects/RegisterList.tsx
@@ -1,106 +1,49 @@
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  Button,
-} from "@/components/ui";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui";
+import Image from "next/image";
+
+const projects = [
+  {
+    title: "Registration with Lembaga Arkitek Malaysia",
+    imgSrc: "/2024_LAM ARKSB CERTIFICATE.png",
+    altText: "Registration with Lembaga Arkitek Malaysia",
+  },
+  {
+    title: "Registration with SSM",
+    imgSrc: "/Perakuan perbadanan syarikat sendirian_ARKSB.png",
+    altText: "Registration with SSM",
+  },
+  {
+    title: "Registration with Kementerian Kewangan Malaysia",
+    imgSrc: "/MOF Cert_2022 to 2025.png",
+    altText: "Registration with Kementerian Kewangan Malaysia",
+  },
+  {
+    title: "Registration with Jabatan Kastam DiRaja Malaysia",
+    imgSrc: "/KASTAM Approval Letter_SST.png",
+    altText: "Registration with Jabatan Kastam DiRaja Malaysia",
+  },
+];
 
 export default function ProjectsList() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center w-full">
       <div className="grid gap-y-8 gap-x-8 sm:grid-cols-2 lg:grid-cols-3 w-full">
-        <Card className="w-full h-full">
-          <CardHeader>
-            <CardTitle>Registration with Lembaga Arkitek Malaysia</CardTitle>
-            <Button
-              onClick={() =>
-                window.open("/2024_LAM ARKSB CERTIFICATE.pdf", "_blank")
-              }
-            >
-              View in Full Screen
-            </Button>
-          </CardHeader>
-          <CardContent className="w-full h-full">
-            <iframe
-              src="/2024_LAM ARKSB CERTIFICATE.pdf#zoom=100"
-              width="100%"
-              height="100%"
-              className="border w-full h-[500px]"
-            ></iframe>
-          </CardContent>
-        </Card>
-
-        <Card className="w-full h-full">
-          <CardHeader>
-            <CardTitle>Registration with SSM</CardTitle>
-            <Button
-              onClick={() =>
-                window.open(
-                  "/Perakuan perbadanan syarikat sendirian_ARKSB.pdf",
-                  "_blank"
-                )
-              }
-            >
-              View in Full Screen
-            </Button>
-          </CardHeader>
-          <CardContent className="w-full h-full">
-            <iframe
-              src="/Perakuan perbadanan syarikat sendirian_ARKSB.pdf#zoom=100"
-              width="100%"
-              height="100%"
-              className="border w-full h-[500px]"
-            ></iframe>
-          </CardContent>
-        </Card>
-
-        <Card className="w-full h-full">
-          <CardHeader>
-            <CardTitle>
-              Registration with Kementerian Kewangan Malaysia
-            </CardTitle>
-            <Button
-              onClick={() =>
-                window.open("/MOF Cert_2022 to 2025.pdf", "_blank")
-              }
-            >
-              View in Full Screen
-            </Button>
-          </CardHeader>
-          <CardContent className="w-full h-full">
-            <iframe
-              src="/MOF Cert_2022 to 2025.pdf#zoom=100"
-              width="100%"
-              height="100%"
-              className="border w-full h-[500px]"
-            ></iframe>
-          </CardContent>
-        </Card>
-
-        <Card className="w-full h-full">
-          <CardHeader>
-            <CardTitle>
-              Registration with Jabatan Kastam DiRaja Malaysia
-            </CardTitle>
-            <Button
-              onClick={() =>
-                window.open("/KASTAM Approval Letter_SST.pdf", "_blank")
-              }
-            >
-              View in Full Screen
-            </Button>
-          </CardHeader>
-
-          <CardContent className="w-full h-full">
-            <iframe
-              src="/KASTAM Approval Letter_SST.pdf#zoom=100"
-              width="100%"
-              height="100%"
-              className="border w-full h-[500px]"
-            ></iframe>
-          </CardContent>
-        </Card>
+        {projects.map((project, index) => (
+          <Card key={index} className="w-full h-full pb-5">
+            <CardHeader>
+              <CardTitle>{project.title}</CardTitle>
+            </CardHeader>
+            <CardContent className="w-full h-full flex justify-center items-center">
+              <Image
+                src={project.imgSrc} // Image source
+                alt={project.altText} // Alt text for the image
+                width={800} // Specify width
+                height={500} // Specify height
+                className="object-contain border" // Adjust styling as needed
+              />
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </div>
   );
