941be49c94468267ad4744f49d77dc136819ddba
Aiyern30 <ianbian2@gmail.com>
2025-03-07 19:56:07 +0800

    Install sonner and added google map components with toast message

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "sonner";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
 
-      <main className="mt-[80px]">{children}</main>
+      <main className="mt-[80px]">
+        {children}
+        <Toaster />
+      </main>
     </div>
   );
 };
