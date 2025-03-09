f70fb7b7378924f8e8475b09160ee0abdf0815b1
Aiyern30 <ianbian2@gmail.com>
2025-03-09 11:14:10 +0800

    `Update image URLs for Director Mazli to include "Directors" directory`
    or
    
    `Update image URLs for Director Mazli`

--- components/pages/Directors/DirectorSection.tsx
+++ components/pages/Directors/DirectorSection.tsx
@@ -41,7 +41,7 @@ const UserDetails: Director[] = [
   },
   {
     name: "AR. MAZLI BIN MD. LAJIS",
-    imageURL: "/Mazli.jpg",
+    imageURL: "/Directors/Mazli.jpg",
     role: "Director",
     description: [
       "1994-1995 joined Juhari Hashim Arkitek as Assistance Architect before further studies.",
